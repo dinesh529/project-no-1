7460fc546067a5ce6499dc0c5dc3f6c2c8f31445
DINESH K <124152479+dinesh529@users.noreply.github.com>
2026-05-27 11:31:52 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request
+import time
+
+app = Flask(__name__)
+
+def function(txt, pat, n, m):
+    for i in range(n - m + 1):
+        if txt[i:m+i] == pat:
+            return i
+    return -1
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    result = None
+    time_taken = None
+
+    if request.method == 'POST':
+        txt = request.form['txt']
+        pat = request.form['pat']
+
+        stime = time.time()
+
+        result = function(txt, pat, len(txt), len(pat))
+
+        etime = time.time()
+
+        time_taken = etime - stime
+
+    return render_template('index.html',
+                           result=result,
+                           time_taken=time_taken)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
